1b8e395a20148242d37c6f778003b961580988b7
Alessandro Umbrico <umbrico.alessandro@gmail.com>
2019-07-25 10:23:55 +0200

    adding executive dc testing

--- src/main/java/it/istc/pst/platinum/executive/dc/DCExecutive.java
+++ src/main/java/it/istc/pst/platinum/executive/dc/DCExecutive.java
@@ -4,7 +4,6 @@ import it.istc.pst.platinum.executive.Executive;
 import it.istc.pst.platinum.executive.dc.strategy.Strategy;
 import it.istc.pst.platinum.executive.dc.strategy.loader.StrategyLoader;
 import it.istc.pst.platinum.executive.monitor.ConditionCheckingMonitor;
-import it.istc.pst.platinum.executive.pdb.ExecutionNode;
 import it.istc.pst.platinum.framework.microkernel.annotation.cfg.FrameworkLoggerConfiguration;
 import it.istc.pst.platinum.framework.microkernel.annotation.cfg.executive.DispatcherConfiguration;
 import it.istc.pst.platinum.framework.microkernel.annotation.cfg.executive.MonitorConfiguration;
